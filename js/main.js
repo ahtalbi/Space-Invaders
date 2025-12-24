52d38895378c1ca5efc461f401b3df57eea7fb12
bnomenja <kyler6612@gmail.com>
2025-12-24 15:48:25 +0100

    adding background

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import { createContainer } from './map.js'
+
+createContainer()
---
+++ js/map.js
@@ -0,0 +1,7 @@
+export const createContainer = () => {
+    const container = document.createElement("div")
+    container.className = "container"
+
+    document.body.append(container)
+
+}
