152c757070c7b54ef65ceb96feb60a943a02aab6
ahtalbi <obetox1@gmail.com>
2025-12-25 20:14:27 +0100

    afficher tout les enemies

--- js/enemies.js
+++ js/enemies.js
@@ -1,24 +1,48 @@
 let configEnemies = {
     speed: 40,
     numberEnemies: 15,
-    conteurCreation: 1,
+    width: 150,
     leftWall: 0,
     rightWall: 0,
-    bottomWall: 0,
+    arrOfEnemies: [],
 }
 
+// this represent the enemy object, it can create and update the enemy position, also includes live or dead
 export class enemy {
+    // creates the enemy DOM element and sets its base styles
     constructor() {
         this.enemyElement = document.createElement("img");
         this.enemyElement.src = "assets/pictures/enemies/pixil-frame-0.png";
         this.enemyElement.style.position = "absolute";
-        this.enemyElement.style.width = "200px";
+        this.enemyElement.style.width = `${configEnemies.width}px`;
+        this.enemyElement.style.height = `auto`;
+        this.enemyElement.style.top = "0px";
+        this.enemyElement.style.left = "0px";
         this.enemyElement.style.imageRendering = "pixelated";
     }
-    create(container) {
-        if (configEnemies.conteurCreation > configEnemies.numberEnemies) return null;
+
+    // creates the enemy and places it relative to the previous one
+    create(container, num) {
+        if (num !== 0) {
+            console.log(configEnemies.arrOfEnemies[num - 1].style.top, configEnemies.arrOfEnemies[num - 1].style.left);
+            if (num % 5 === 0) {
+                this.enemyElement.style.top = `${parseInt(configEnemies.arrOfEnemies[num - 1].style.top, 10) + configEnemies.width/4*2}px`;
+                this.enemyElement.style.left = `0px`;
+            } else {
+                this.enemyElement.style.top = `${parseInt(configEnemies.arrOfEnemies[num - 1].style.top, 10)}px`;
+                this.enemyElement.style.left = `${parseInt(configEnemies.arrOfEnemies[num - 1].style.left, 10) + configEnemies.width/4*3}px`;
+            }
+        }
         container.append(this.enemyElement);
-        return true;
     }
 }
 
+// this function is for initialize and create all enemies
+export function InitlizeTheEnemies(container) {
+    for (let i = 0; i < configEnemies.numberEnemies; i++) {
+        let en = new enemy();
+        en.create(container, i);
+        configEnemies.arrOfEnemies.push(en.enemyElement);
+    }
+
+}
--- js/map.js
+++ js/map.js
@@ -1,14 +1,14 @@
 import { Rocket } from "./fusée.js";
-import {  enemy } from "./enemies.js";
+import { InitlizeTheEnemies } from "./enemies.js";
 export let rocket = null
 
 export const createContainer = async () => {
   const container = document.createElement("div");
   container.className = "container";
-
   document.body.append(container);
+  
+  InitlizeTheEnemies(container);
+  
   rocket = new Rocket()
   rocket.create(container)
-  let en = new enemy()
-  en.create(container);
 }
